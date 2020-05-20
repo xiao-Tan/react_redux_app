f32c8921828cc6ccecc0329680016bc8f984cea3
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-05-20 14:46:13 -0700

    first action

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
 
 class CoursesPage extends Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  //用arrow function 可以不用bind this
+  changeHandler = (e) => {
+    const course = { ...this.state.course, title: e.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h1>Courses Page</h1>;
+    return (
+      <div>
+        <h1>Courses Page</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.changeHandler}
+            value={this.state.course.title}
+          />
+          <button type="submit">Add course</button>
+        </form>
+      </div>
+    );
   }
 }
 
---
+++ src/redux/actions/courseActions.js
@@ -0,0 +1,6 @@
+export function createCourse(course) {
+  return { type: "CREATE_COURSE", course };
+}
+
+//1,必须有type property
+//2,porperty 2 是 action payload： course: course 当key和value一样时，可以省略value
