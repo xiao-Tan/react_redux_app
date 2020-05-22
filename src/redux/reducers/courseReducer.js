85be3c4d673121c14e963ddf1fe4d6c2c03bd1a9
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-05-22 14:20:35 -0700

    initialState to see store's shape

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "../actions/types";
+import initialState from "./initialState";
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case CREATE_COURSE:
       //debugger;
