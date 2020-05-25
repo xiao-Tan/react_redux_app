b5a0a3ecefa50e00fea1fda2a13f604493fd2064
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-05-25 14:07:33 -0700

    add saveOrUpdateCourse thunk with new action and reducer

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,13 +1,20 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "./types";
-import { getCourses } from "../../api/courseApi";
-
-export function createCourse(course) {
-  //debugger;
-  return { type: CREATE_COURSE, course };
-}
+import {
+  LOAD_COURSES_SUCCESS,
+  CREATE_COURSE_SUCCESS,
+  UPDATE_COURSE_SUCCESS,
+} from "./types";
+import { getCourses, saveCourse } from "../../api/courseApi";
 
 export function loadCoursesSuccess(courses) {
   return { type: LOAD_COURSES_SUCCESS, courses };
+}
+
+export function createCourseSuccess(course) {
+  return { type: CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: UPDATE_COURSE_SUCCESS, course };
 }
 
 //1,必须有type property
@@ -19,6 +26,20 @@ export function loadCourses() {
     return getCourses()
       .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function addOrUpdateCourse(course) {
+  return function (dispatch) {
+    return saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,11 +1,19 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "../actions/types";
+import {
+  LOAD_COURSES_SUCCESS,
+  CREATE_COURSE_SUCCESS,
+  UPDATE_COURSE_SUCCESS,
+} from "../actions/types";
 import initialState from "./initialState";
 
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
-    case CREATE_COURSE:
-      //debugger;
+    case CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
+    case UPDATE_COURSE_SUCCESS:
+      //immutable, so map return new array, replacing the element with matching course.id
+      return state.map((course) =>
+        course.id === action.course.id ? action.course : course
+      );
     case LOAD_COURSES_SUCCESS:
       return action.courses;
     default:
